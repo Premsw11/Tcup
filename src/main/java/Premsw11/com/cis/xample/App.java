e65c7e0e6f000e662596c7e8eeb02518b0a66a09
premsw11 <premsw11@gmail.com>
2017-06-12 12:59:43 +0530

    "commit the project"

---
+++ src/main/java/Premsw11/com/cis/xample/App.java
@@ -0,0 +1,13 @@
+package Premsw11.com.cis.xample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Premsw11/com/cis/xample/sam.java
@@ -0,0 +1,15 @@
+package Premsw11.com.cis.xample;
+
+public class sam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Practice git");
+	}
+
+}
+
+
+
+
+
